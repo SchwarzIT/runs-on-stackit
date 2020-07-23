6d819d449040a04e3b6e762e3ee24a720195c220
Engin Diri <engin.diri@lidl.com>
2020-07-23 17:45:42 +0200

    fix console.log

--- src/components/stackit-badge/stackit-badge.tsx
+++ src/components/stackit-badge/stackit-badge.tsx
@@ -65,7 +65,6 @@ export class STACKITBadgeComponent {
   }
 
   private getHostType() {
-    console.log(this.hostingType)
     switch (this.hostingType) {
       case 'runson':
         return 'Runs-On'
